d32ea74f10527344bbdaa5cbb71ae5c2904068c3
Syed Hisham Akmal <hisham0502@gmail.com>
2023-10-30 00:20:58 +0530

    Added relevant documentation in handlers/api.go

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -17,3 +17,11 @@ func Handler(r *chi.Mux){
 		router.Get("/coins", GetCoinBalance)
 	})
 }
+
+//Difference between chi.Mux vs chi.Router
+
+// chi.Router: chi.Router is an interface in the Chi package that defines the common methods for routing in Chi. It includes methods for defining routes, applying middleware, and other routing features. It serves as the basis for routing in Chi.
+
+// chi.Mux: chi.Mux is the primary and most commonly used router implementation in Chi. It is a concrete type that implements the chi.Router interface. chi.Mux provides a wide range of routing capabilities, including routing, middleware, subrouting, and more. It is suitable for most web application use cases.
+
+// To summarize, chi.Mux is the primary router implementation in Chi, while chi.Router is an interface that defines the common routing methods. You would typically use chi.Mux as your primary router when building web applications with Chi.
