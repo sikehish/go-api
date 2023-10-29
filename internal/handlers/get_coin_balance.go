46be232402d69dc71db2e446454f8a795e9d65e2
Syed Hisham Akmal <hisham0502@gmail.com>
2023-10-30 00:21:38 +0530

    Made a minor change with respect to a pointer

--- internal/handlers/get_coin_balance.go
+++ internal/handlers/get_coin_balance.go
@@ -38,7 +38,7 @@ func GetCoinBalance(w http.ResponseWriter, r *http.Request){
 	}
 
 	var response=api.CoinBalanceResponse{
-		Balance:(*tokenDetails).Coins,
+		Balance:tokenDetails.Coins,
 		Code: http.StatusOK,
 	}
 
