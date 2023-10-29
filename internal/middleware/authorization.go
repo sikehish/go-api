fb68e1e0957ee26fd923397f9134324deb3bfc74
Syed Hisham Akmal <hisham0502@gmail.com>
2023-10-30 00:22:26 +0530

    Documentation in authorization.go, database.go

--- internal/middleware/authorization.go
+++ internal/middleware/authorization.go
@@ -14,6 +14,17 @@ var unAuthorizedError= errors.New("Invalid username or token.")
 //Authorization middleware
 func Authorization(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+
+		//NOTE:
+		// In Go, http.ResponseWriter is not typically taken as a pointer because it is an interface. Go interfaces are already reference types, and you don't need a pointer to modify the value that implements an interface.  You can pass an interface by value (not as a pointer) and still make modifications to the underlying object.you use it to write the response (e.g., writing headers and response content). The implementations of the interface take care of modifying the response based on the methods you call.
+
+	
+	// In this code, the Authorization middleware function is designed to return an http.Handler that performs authorization checks before allowing the actual request handling to proceed. Let's break down how this works:
+
+    // http.HandlerFunc is an adapter function. It converts a regular Go function with the signature func(http.ResponseWriter, *http.Request) into an http.Handler, which is necessary for middleware to fit into the Go net/http middleware chain.
+
+    // The Authorization middleware function is defined to return an http.Handler. It takes another http.Handler (next) as an argument, which is typically the next handler in the chain, and wraps it with authorization logic.
+
 		//Get username and token from header
 		var username string=r.URL.Query().Get("username")
 		var token=r.Header.Get("Authorization")
@@ -36,13 +47,17 @@ func Authorization(next http.Handler) http.Handler {
 		var loginDetails *tools.LoginDetails
 		loginDetails=(*database).GetUserLoginDetails(username)
 
-		if (loginDetails==nil || (token!=(*loginDetails).AuthToken)){
+			//Note: n Go, both -> and . are represented by ""." .The compiler knows the types, and can dereference if necessary.
+			//loginDetails.AuthToken == (*loginDetails).AuthToken
+		if (loginDetails==nil || (token!=loginDetails.AuthToken)){
 			log.Error(unAuthorizedError)
 			api.RequestErrorHandler(w, unAuthorizedError)
 			return
 		}
 
 		next.ServeHTTP(w,r)
+		// The next.ServeHTTP(w, r) call is used to pass the control of the HTTP request to the next handler in the middleware chain. It allows the request to continue processing by the subsequent handlers in the chain.
+		// When you call next.ServeHTTP(w, r) within the Authorization middleware, you're effectively passing the http.ResponseWriter and *http.Request objects to the next handler in the middleware chain. This means that after the Authorization middleware performs its checks and logic, it allows the subsequent handler (e.g., your application's request handler) to process the request.
 
 	})
 }
--- internal/tools/database.go
+++ internal/tools/database.go
@@ -22,6 +22,12 @@ type DatabaseInterface interface{
 func NewDatabase() (*DatabaseInterface, error){
 	var database DatabaseInterface=&mockDB{}
 
+// 	In Go, interfaces are typically satisfied using pointers to types to avoid unnecessary copying of values. This also allows the database variable to hold any value that implements the DatabaseInterface interface, whether it's a pointer to a struct or an actual struct, as long as the methods are implemented correctly.
+
+// You can't directly pass mockDB to database because mockDB is a struct type, not an interface type. To assign it to an interface variable, you need to create an instance of it and use a pointer to that instance to satisfy the interface. This is a common pattern in Go when working with interfaces and their concrete implementations.
+
+//Interface is a reference type in go
+
 	var err error=database.SetupDatabase()
 	if err!=nil{
 		log.Error(err)
